6352894ac4ddf5f2b76b7fba9002744b70c439c5
tninh <tri.ninh01@gmail.com>
2018-09-11 17:45:20 -0700

    Add sorted list of contacts

--- App.js
+++ App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 import {Constants} from 'expo'
 
-import contacts from './contacts'
+import contacts, {compareNames} from './contacts'
+
+import Row from './Row'
 
 export default class App extends React.Component {
-  state = {
-    showContacts: false,
-  }
-
+  
+    state = {
+      showContacts: false,
+      contacts: contacts
+    }
+  
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
   }
 
+  sort = () => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts].sort(compareNames)
+    }))
+  }
+
+
+  renderItem = ({item}) => <Row {...item} />
+
   render() {
+    
     return (
       <View style={styles.container}>
         <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="sort" onPress={this.sort} />
+        {this.state.showContacts && (
+          <FlatList 
+          renderItem={this.renderItem}
+          data = {this.state.contacts} />
+        )
+        }
       </View>
     );
+    
   }
 }
 
