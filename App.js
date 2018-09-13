1744073225a48b0e30a0a5139a807e5638786c70
tninh <tri.ninh01@gmail.com>
2018-09-12 19:47:28 -0700

    Add contact form

--- App.js
+++ App.js
@@ -8,15 +8,22 @@ import Row from './Row'
 
 import ContactsList from './ContactsList'
 
+import AddContactForm from './AddContactForm'
+
 export default class App extends React.Component {
   
     state = {
       showContacts: false,
+      showForm: false,
       contacts: contacts
     }
   
   toggleContacts = () => {
     this.setState(prevState => ({showContacts: !prevState.showContacts}))
+  }
+
+  toggleForm = () => {
+    this.setState(prevState => ({showForm: !prevState.showForm}))
   }
 
   sort = () => {
@@ -29,10 +36,12 @@ export default class App extends React.Component {
   
 
   render() {
+    if (this.state.showForm) return <AddContactForm />
     
     return (
       <View style={styles.container}>
         <Button title="toggle contacts" onPress={this.toggleContacts} />
+        <Button title="Add Contact" onPress={this.toggleForm} />
         <Button title="sort" onPress={this.sort} />
           {this.state.showContacts && <ContactsList contacts={this.state.contacts} />
         
